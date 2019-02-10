8a89e92213cdf4812bb0e44e98e016059a2fd02e
Student <localhost>
2019-02-10 11:04:11 +0300

    coverage added

--- app/lib.py
+++ app/lib.py
@@ -1,16 +1,8 @@
 def is_creditable(age, salary):
     '''
-    >>> is_creditable(30, 40_000)
-    True
-
     >>> is_creditable(20, 40_000)
     False
 
-    >>> is_creditable(70, 40_000)
-    False
-
-    >>> is_creditable(30, 10_000)
-    False
 
     '''
     min_age = 21
